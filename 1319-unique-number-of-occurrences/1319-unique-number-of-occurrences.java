e927a6861019a1372e713f631260fd62dc25ea2c
Anirudh Kalghatkar <75170111+Anirudhpk2001@users.noreply.github.com>
2023-09-28 02:33:34 -0600

    Time: 2 ms (97.75%), Space: 40.8 MB (31%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
@@ -1,18 +1,24 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        HashMap<Integer,Integer> Map=new HashMap<>();
-        Set<Integer> out=new HashSet<>();
+        // Create a HashMap to store the count of occurrences for each number.
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        // Create a HashSet to store unique counts.
+        Set<Integer> out = new HashSet<>();
     
-        for(int i=0;i<arr.length;i++)
-        {
-           Map.put(arr[i],Map.getOrDefault(arr[i],0)+1);
+        // Iterate through the array to count occurrences.
+        for(int i = 0; i < arr.length; i++) {
+            // Use getOrDefault to safely increment the count.
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
         }
-        for(int num:Map.values())
-        {
+        
+        // Iterate through the values in the map (occurrence counts).
+        for(int num : map.values()) {
             out.add(num);
         }
         
-        return Map.size()!=out.size() ? false :true;
-
+        // Check if the number of unique occurrences matches the number of unique counts.
+        // If they are equal, return true; otherwise, return false.
+        return map.size() != out.size() ? false : true;
     }
 }
