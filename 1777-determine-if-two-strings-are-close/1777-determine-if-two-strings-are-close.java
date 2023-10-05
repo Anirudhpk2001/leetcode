56a89b9a6d4143273b02b82cc79401fffe2c5758
Anirudh Kalghatkar <75170111+Anirudhpk2001@users.noreply.github.com>
2023-10-05 00:49:41 -0600

    Time: 117 ms (5.29%), Space: 44.5 MB (65.55%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.java
@@ -1,40 +1,72 @@
+import java.util.Collection;
+
 class Solution {
     public boolean closeStrings(String word1, String word2) {
         HashMap<Character,Integer> map1=new HashMap<>();
         HashMap<Character,Integer> map2=new HashMap<>();
-        if(word1.length()!=word2.length())
+         
+
+        if(word2.length()!=word1.length())
+        {
+            return false;
+        }
+        for(int i=0;i<word1.length();i++)
+        { if(map1.containsKey(word1.charAt(i)))
+            {
+                map1.put(word1.charAt(i),map1.get(word1.charAt(i))+1);
+            }
+            else{
+                map1.put(word1.charAt(i),1);
+            }
+            if(map2.containsKey(word2.charAt(i)))
+            {
+                map2.put(word2.charAt(i),map2.get(word2.charAt(i))+1); 
+            }
+            else{
+                map2.put(word2.charAt(i),1);
+            }
+        }
+        for(int i=0;i<word1.length();i++)
+        {
+            if(!map2.containsKey(word1.charAt(i)))
+            {
+                return false;
+            }
+            if(!map1.containsKey(word2.charAt(i)))
+            {
+                return false;
+            }
+        }
+        // for(int n : map1.values())
+        // {
+        //     if(!map2.containsValue(n))
+        //     {
+        //         return false;
+        //     }
+        // }
+
+
+        // for(int k : map2.values())
+        // {
+        //     if(!map1.containsValue(k))
+        //     {
+        //         return false;
+        //     }
+        // }  
+         Collection<Integer> values1 = map1.values();
+         Collection<Integer>  values2=map2.values();
+  
+        ArrayList<Integer> listOfValues1 = new ArrayList<>(values1);
+        ArrayList<Integer> listOfValues2=new ArrayList<>(values2);
+        Collections.sort(listOfValues1);
+        Collections.sort(listOfValues2);
+
+        if(!listOfValues1.equals(listOfValues2))
         {
             return false;
         }
 
-        for(char  c:word1.toCharArray())
-        {
-            map1.put(c,map1.getOrDefault(c,0)+1);
-        }
 
-        for(char  c:word2.toCharArray())
-        {   
-            if(!map1.containsKey(c))
-            {
-                return false;
-            }
-            map2.put(c,map2.getOrDefault(c,0)+1);
-        }
-
-        HashMap<Integer,Integer> hm1=new HashMap<>();
-        HashMap<Integer,Integer> hm2=new HashMap<>();
-
-        for(int freq:map1.values())
-        {
-            hm1.put(freq,hm1.getOrDefault(freq,0)+1);
-        }
-        for(int freq:map2.values())
-        {
-            hm2.put(freq,hm2.getOrDefault(freq,0)+1);
-        }
-
-        return hm1.equals(hm2);
-
-        
+        return true;
     }
 }
