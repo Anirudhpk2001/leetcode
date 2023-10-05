ccad249c8be4a90ca0ef04709d3af2cdf9460b45
Anirudh Kalghatkar <75170111+Anirudhpk2001@users.noreply.github.com>
2023-10-05 00:45:39 -0600

    Time: 71 ms (38.04%), Space: 44.4 MB (78.48%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.java
@@ -1,42 +1,40 @@
-import java.util.HashMap;
-
 class Solution {
     public boolean closeStrings(String word1, String word2) {
-        HashMap<Character, Integer> map1 = new HashMap<>();
-        HashMap<Character, Integer> map2 = new HashMap<>();
-
-        if (word2.length() != word1.length()) {
+        HashMap<Character,Integer> map1=new HashMap<>();
+        HashMap<Character,Integer> map2=new HashMap<>();
+        if(word1.length()!=word2.length())
+        {
             return false;
         }
 
-        for (char c : word1.toCharArray()) {
-            map1.put(c, map1.getOrDefault(c, 0) + 1);
+        for(char  c:word1.toCharArray())
+        {
+            map1.put(c,map1.getOrDefault(c,0)+1);
         }
 
-        for (char c : word2.toCharArray()) {
-            if (!map1.containsKey(c)) {
-                return false;  // Character sets are not the same
+        for(char  c:word2.toCharArray())
+        {   
+            if(!map1.containsKey(c))
+            {
+                return false;
             }
-            map2.put(c, map2.getOrDefault(c, 0) + 1);
+            map2.put(c,map2.getOrDefault(c,0)+1);
         }
 
-        // Check if the sets of characters are the same
-        if (!map1.keySet().equals(map2.keySet())) {
-            return false;
+        HashMap<Integer,Integer> hm1=new HashMap<>();
+        HashMap<Integer,Integer> hm2=new HashMap<>();
+
+        for(int freq:map1.values())
+        {
+            hm1.put(freq,hm1.getOrDefault(freq,0)+1);
+        }
+        for(int freq:map2.values())
+        {
+            hm2.put(freq,hm2.getOrDefault(freq,0)+1);
         }
 
-        // Check if the frequencies are the same
-        HashMap<Integer, Integer> freqCountMap1 = new HashMap<>();
-        HashMap<Integer, Integer> freqCountMap2 = new HashMap<>();
+        return hm1.equals(hm2);
 
-        for (int freq : map1.values()) {
-            freqCountMap1.put(freq, freqCountMap1.getOrDefault(freq, 0) + 1);
-        }
-
-        for (int freq : map2.values()) {
-            freqCountMap2.put(freq, freqCountMap2.getOrDefault(freq, 0) + 1);
-        }
-
-        return freqCountMap1.equals(freqCountMap2);
+        
     }
 }
