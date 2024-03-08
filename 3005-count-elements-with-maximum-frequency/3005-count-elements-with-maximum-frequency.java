0f465c75bb7210ac94322caf368d0c2ab2039ee7
Anirudh Kalghatkar <75170111+Anirudhpk2001@users.noreply.github.com>
2024-03-07 17:54:27 -0700

    Time: 3 ms (31.20%), Space: 42.4 MB (39.79%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
@@ -6,10 +6,7 @@ class Solution {
         for(int i=0;i<nums.length;i++)
         {
             hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
-        }
-        for(int val:hm.values())
-        {
-            maxval=Math.max(maxval,val);
+            maxval=Math.max(maxval,hm.get(nums[i]));
         }
         for(int val:hm.values())
         {
