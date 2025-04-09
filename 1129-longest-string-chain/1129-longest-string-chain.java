a4b784ca327b5a14fd05fb83cc677edf1dee3d7a
Anirudh Kalghatkar <75170111+Anirudhpk2001@users.noreply.github.com>
2025-04-08 23:21:14 -0600

    Time: 48 ms (51.55%), Space: 44.4 MB (71.92%) - LeetHub

---
+++ 1129-longest-string-chain/1129-longest-string-chain.java
@@ -0,0 +1,53 @@
+class Solution {
+    private boolean isValid(String word1,String word2)
+    {
+        int i=0;
+        int j=0;
+        int n=word1.length();
+        int m=word2.length();
+        if(m!=n-1)
+        {
+            return false;
+        }
+        while(i<n)
+        {
+           
+            if(j<m && word1.charAt(i)==word2.charAt(j))
+            {
+                i++;
+                j++;
+            }
+            else
+            {
+                 i++;
+            }
+           
+
+        }
+
+
+        return j==m;
+    }
+    public int longestStrChain(String[] words) {
+        Arrays.sort(words,(a,b)-> Integer.compare(a.length(),b.length()));
+
+        int t[] =new int[words.length];
+        Arrays.fill(t,1);
+        int max=0;
+        for(int i=0;i<words.length;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                if(isValid(words[i],words[j]))
+                {
+                    t[i]=Math.max(t[i],1+t[j]);
+
+                }
+                
+            }
+            max=Math.max(t[i],max);
+        }
+
+        return max;
+    }
+}
