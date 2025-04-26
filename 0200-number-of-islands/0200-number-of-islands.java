d92dae7eaa93f218a89c6a2f8b89596c78639d67
Anirudh Kalghatkar <75170111+Anirudhpk2001@users.noreply.github.com>
2025-04-25 23:39:14 -0600

    Time: 3 ms (84.63%), Space: 50.2 MB (28.2%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,43 +1,49 @@
 class Solution {
-    int num_island=0;
-    public void DFS(char[][] grid ,int i,int j ){
-        
-        grid[i][j]='2';
-        if(i>0 && grid[i-1][j]=='1')
-        {
-            DFS(grid,i-1,j);
-        }
-        if(i<grid.length-1 && grid[i+1][j]=='1')
-        {
-            DFS(grid,i+1,j);
-        }
-        if(j>0 && grid[i][j-1]=='1')
-        {
-             DFS(grid,i,j-1);
-        }
-         if(j<grid[0].length-1 && grid[i][j+1]=='1')
-        {
-            DFS(grid,i,j+1);
-        }
-        
+    int[][] directions;
+    int m;
+    int n;
+    private boolean isValid(int x,int y)
+    {
+        return x>=0 && y>=0 && x<m && y<n;
     }
-    
+    private void dfs(char[][] grid ,int x,int y )
+    {
+        if(!isValid(x,y))
+        {
+            return;
+        }
+
+        grid[x][y]='2';
+
+        for(int[] dir:directions)
+        {
+            int nx=x+dir[0];
+            int ny=y+dir[1];
+            if(isValid(nx,ny) && grid[nx][ny]=='1')
+            {
+                dfs(grid,nx,ny);
+            }
+            
+        }
+    }
     public int numIslands(char[][] grid) {
-        
-        
-        Stack<Pair<Integer,Integer>> st=new Stack<>(); 
-        
+        directions=new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
+        m=grid.length;
+        n=grid[0].length;
+        int Num_of_Islands=0;
         for(int i=0;i<grid.length;i++)
         {
             for(int j=0;j<grid[0].length;j++)
             {
                 if(grid[i][j]=='1')
-                {   
-                    num_island++;
-                   DFS(grid,i,j)   ;
+                {
+                    Num_of_Islands++;
+                    dfs(grid,i,j);
                 }
             }
         }
-        return num_island;
+
+        return Num_of_Islands;
+
     }
 }
