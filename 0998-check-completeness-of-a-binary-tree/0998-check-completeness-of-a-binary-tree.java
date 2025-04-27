3eba6414aba53744ff34e228fe8a8ef39f8ab7e6
Anirudh Kalghatkar <75170111+Anirudhpk2001@users.noreply.github.com>
2025-04-27 00:50:54 -0600

    Time: 0 ms (100%), Space: 42.4 MB (31.92%) - LeetHub

--- 0998-check-completeness-of-a-binary-tree/0998-check-completeness-of-a-binary-tree.java
+++ 0998-check-completeness-of-a-binary-tree/0998-check-completeness-of-a-binary-tree.java
@@ -14,39 +14,35 @@
  * }
  */
 class Solution {
-    public boolean isCompleteTree(TreeNode root) {
 
-        Queue<TreeNode> q=new LinkedList<>();
-
-        q.add(root);
-        boolean seenNull=false;
-
-        while(!q.isEmpty())
+    private int count(TreeNode root)
+    {
+        if(root==null)
         {
-            TreeNode temp = q.poll();
-            if(temp==null)
-            {
-                seenNull=true;
-            }
-            else
-            {
-                if(seenNull==true)
-                {
-                    return false;
-                }
-                q.add(temp.left);
-                q.add(temp.right);
-            }
-           
-
-           
-           
+            return 0;
         }
-         return true;
-   
-   
-   
+
+        return 1+count(root.left)+count(root.right);
     }
 
-   
+
+    private boolean dfs(TreeNode root, int i,int totalNodes)
+    {
+
+        if(root==null)
+        {
+            return true;
+        }
+
+        if(i>totalNodes)
+        {
+            return false;
+        }
+
+        return dfs(root.left,2*i,totalNodes) && dfs(root.right,2*i+1,totalNodes);
+    }
+    public boolean isCompleteTree(TreeNode root) {
+        int totalNodes=count(root);
+        return dfs(root,1,totalNodes);
+    }
 }
