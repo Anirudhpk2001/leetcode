178e5b612575f353f5b7040374c2ef15f2497efa
Anirudh Kalghatkar <75170111+Anirudhpk2001@users.noreply.github.com>
2025-04-29 01:16:43 -0600

    Time: 6 ms (5.15%), Space: 41.7 MB (40.99%) - LeetHub

--- 0904-leaf-similar-trees/0904-leaf-similar-trees.java
+++ 0904-leaf-similar-trees/0904-leaf-similar-trees.java
@@ -14,43 +14,30 @@
  * }
  */
 class Solution {
-    public ArrayList<Integer> check1(TreeNode root1,ArrayList<Integer> a1)
-    {   if(root1==null)
+    private void dfs(TreeNode root,StringBuilder str)
+    {
+        if(root==null)
         {
-            return null;
+            return;
         }
-        if(root1.left==null && root1.right==null){
-            a1.add(root1.val);
-            return a1;
-        }
-        check1(root1.left,a1);
-        check1(root1.right,a1);
-        return a1;
-    }
-    public ArrayList<Integer> check2(TreeNode root2,ArrayList<Integer> a2)
-    {   if(root2==null)
+
+        if(root.left==null && root.right==null)
         {
-            return null;
-        }
-        
-        if(root2.left==null && root2.right==null){
-            a2.add(root2.val);
-            return a2;
-        }
-        check2(root2.left,a2);
-        check2(root2.right,a2);
-        return a2;
+            str.append(" "+root.val);
+        } 
+
+        dfs(root.left,str);
+        dfs(root.right,str);
     }
     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
-        
-        
-        ArrayList<Integer> a1=new ArrayList<>();
-        ArrayList<Integer> a2=new ArrayList<>();
-        check1(root1,a1);
-        check2(root2,a2);
+        StringBuilder str1=new StringBuilder();
+        StringBuilder str2=new StringBuilder();
 
-        return  a1.equals(a2) ;
+        dfs(root1,str1);
+        dfs(root2,str2);
+        System.out.println(str1);
+        System.out.println(str2);
 
-
+        return str1.toString().equals(str2.toString());
     }
 }
