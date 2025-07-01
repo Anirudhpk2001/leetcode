f0209d499076e98af7d861cd0027f9c4a4c2c5ed
Anirudh Kalghatkar <75170111+Anirudhpk2001@users.noreply.github.com>
2025-07-01 03:58:12 -0600

    Time: 12 ms (83.52%), Space: 44.7 MB (70.38%) - LeetHub

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,37 +1,37 @@
 class Solution {
-    public int maxVowels(String s, int k) {
-        int i=0;
-        int n=s.length();
-        int count=0;
-        int j=k;
-        int max=0;
-        for(int q=0;q<k;q++)
-        {
-            if(s.charAt(q)=='a'||s.charAt(q)=='e'||s.charAt(q)=='i'||s.charAt(q)=='o'||s.charAt(q)=='u')
+
+    private boolean checkVowel(char ch)
+    {
+            if(ch == 'a' ||ch == 'e' ||ch == 'i' ||ch == 'o' ||ch == 'u' )
             {
-                count++;
+                return true;
             }
-
-        }
-
-        max=Math.max(max,count);
-
-        while(j<s.length())
-        {   
-            if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u' )
-            {
-                count--;
-            }
-            if(s.charAt(j)=='a'||s.charAt(j)=='e'||s.charAt(j)=='i'||s.charAt(j)=='o'||s.charAt(j)=='u' )
-            {
-                count++;
-            }
-            i++;
-            j++;
-            max=Math.max(max,count);
-        }
-
-        return max;
+            return false;
     }
+    public int maxVowels(String s, int k) {
+        int maxVowel = 0;
+        int countVowels = 0;
+        for(int i=0;i<k;i++)
+        {
+            if(checkVowel(s.charAt(i)))
+            {
+                countVowels++;
+            }
+        }
 
+        maxVowel = countVowels;
+
+        int i = k;
+
+        while(i<s.length())
+        {
+            int before = (checkVowel(s.charAt(i-k)))? -1:0 ;
+            int after = (checkVowel(s.charAt(i)))? 1:0 ;
+            countVowels = countVowels + before  + after;
+            maxVowel = Math.max(maxVowel, countVowels);
+            i++;
+        }
+
+        return maxVowel;
+    }
 }
