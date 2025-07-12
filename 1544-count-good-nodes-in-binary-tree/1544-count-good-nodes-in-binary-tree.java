a3a51b6d1810a9e581ec9499f13a2ae89a4773ca
Anirudh Kalghatkar <75170111+Anirudhpk2001@users.noreply.github.com>
2025-07-11 23:01:46 -0600

    Time: 2 ms (85.1%), Space: 52.1 MB (93.97%) - LeetHub

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.java
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.java
@@ -14,24 +14,28 @@
  * }
  */
 class Solution {
-    int count=0;
-    public void check(TreeNode root,int max)
-    {   if(root==null  )
+    int count = 0;
+    private void dfs(TreeNode root,int max)
+    {
+        if(root == null)
         {
             return ;
         }
-        if(root.val>=max )
+
+        if(root.val >= max)
         {
-            max = root.val;
             count++;
+            max = root.val;
         }
-        check(root.left,max);
-        
-        check(root.right,max);
-        
+
+        dfs(root.left,max);
+        dfs(root.right,max);
+
+
     }
     public int goodNodes(TreeNode root) {
-        check(root,root.val);
+        
+        dfs(root,root.val);
         return count;
     }
 }
