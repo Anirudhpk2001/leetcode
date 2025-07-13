5d7ebbafd8d27cdb2e97d69af1b686bf63abf88b
Anirudh Kalghatkar <75170111+Anirudhpk2001@users.noreply.github.com>
2025-07-12 22:11:02 -0600

    Time: 11 ms (37.93%), Space: 48.9 MB (16%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findContentChildren(int[] players, int[] trainers) {
+          Arrays.sort(players);
+        Arrays.sort(trainers);
+
+        int i=0;
+        int j = 0;
+
+        while(j<trainers.length && i<players.length)
+        {
+            if(i<players.length && players[i]<= trainers[j])
+            {
+                i++;
+                j++;
+            }
+            else
+            {
+                j++;
+            }
+        }
+
+        return i;
+    }
+}
