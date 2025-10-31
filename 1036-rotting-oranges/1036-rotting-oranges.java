3c7da375367db57af3051ab54b0d1cba9ae04381
Anirudh Kalghatkar <75170111+Anirudhpk2001@users.noreply.github.com>
2025-11-01 00:57:03 +0530

    Time: 2 ms (65.18%), Space: 44.1 MB (5.49%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,80 +1,69 @@
 class Solution {
     int m;
     int n;
-    private boolean isValid(int x , int y)
+    private boolean isValid(int i, int j)
     {
-        if(x>=0 && y>=0 && x<m & y<n)
-        {
-            return true;
-        }
-
-        return false;
+        return (i>=0 && j>=0 && i<m && j<n);
     }
     public int orangesRotting(int[][] grid) {
-        int[][] directions=new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
-        m=grid.length;
-        n=grid[0].length;
-      
-
-        Queue<int[]> q=new LinkedList<>();
-
-        for(int i=0;i<m;i++)
+        m = grid.length;
+        n = grid[0].length;
+        Queue<int[]> q = new LinkedList<>();
+        int fresh = 0;
+        for(int i=0;i<grid.length;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=0;j<grid[0].length;j++)
             {
-                if(grid[i][j]==2)
+                if(grid[i][j] == 1)
+                {
+                    fresh++;
+                }
+                else if(grid[i][j] == 2)
                 {
                     q.add(new int[]{i,j});
                 }
             }
         }
 
-        int count=-1;
+        if(q.isEmpty() )
+        {
+            if(fresh == 0)
+            {
+                return 0;
+            }
+            return -1;
+        }
+        
 
+
+        int minutes = 0;
+        int[][] directions = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
         while(!q.isEmpty())
         {
-            int size=q.size();
-            
-
+            int size = q.size();
             for(int i=0;i<size;i++)
             {
-                int[] temp=q.poll();
-                int x=temp[0];
-                int y=temp[1];
-
-                for(int[] dir:directions)
+                int[] temp = q.poll();
+                for(int[] dir: directions)
                 {
-                    int nx=x+dir[0];
-                    int ny=y+dir[1];
+                    int nx = dir[0] + temp[0];
+                    int ny = dir[1] + temp[1];
 
-
-                    if(isValid(nx,ny) && grid[nx][ny]==1)
+                    if(isValid(nx,ny) && grid[nx][ny] == 1)
                     {
-                        grid[nx][ny]=2;
+                        grid[nx][ny] = 2;
+                        fresh--;
                         q.add(new int[]{nx,ny});
                     }
-
                 }
             }
-            count++;
+            minutes++;
         }
-
-        for(int i=0;i<m;i++)
+        if(fresh>0)
         {
-            for(int j=0;j<n;j++)
-            {
-                if(grid[i][j]==1)
-                {
-                    return -1;
-                }
-            }
+            return -1;
         }
 
-
-        return count==-1?0:count;
-
-
-
-
+        return minutes-1;
     }
 }
