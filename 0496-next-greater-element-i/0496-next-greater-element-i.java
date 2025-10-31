4ee162e9814378f81fcd1b808b01aae672ef6992
Anirudh Kalghatkar <75170111+Anirudhpk2001@users.noreply.github.com>
2025-10-31 12:03:15 +0530

    Time: 3 ms (88.02%), Space: 44.6 MB (20.32%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,52 +1,33 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        
-        Stack<Integer> st=new Stack<>();
+        Stack<Integer> Monotonic = new Stack<>();
 
-        HashMap<Integer,Integer> map=new HashMap<>();
-
-        st.push(-1);
+        HashMap<Integer,Integer> map = new HashMap<>();
 
         for(int i=nums2.length-1;i>=0;i--)
         {
-            
-            if(st.peek()<nums2[i])
+            while(!Monotonic.isEmpty() && nums2[i]>Monotonic.peek())
             {
-                while( !st.empty() && st.peek()<nums2[i] )
-                {
-                    st.pop();
-                
-                }
-                 if(st.empty())
-                {
-                    st.push(nums2[i]);
-                    map.put(nums2[i],-1);
-                    
-                }
-                else
-                {
-                    map.put(nums2[i],st.peek());
-                    st.push(nums2[i]);
-                }
+                Monotonic.pop();
+            }
+            if(Monotonic.isEmpty())
+            {
+                map.put(nums2[i],-1);
             }
             else
             {
-                map.put(nums2[i],st.peek());
-                st.push(nums2[i]);
-
+                map.put(nums2[i],Monotonic.peek());
             }
-            
-              
-            
-            
+            Monotonic.push(nums2[i]);
         }
-        int[] res=new int[nums1.length];
-        for(int i=0;i<nums1.length;i++ )
+
+        int[] result = new int[nums1.length];
+
+        for(int i=0;i<nums1.length;i++)
         {
-            res[i]=map.get(nums1[i]);
+            result[i] = map.get(nums1[i]);
         }
 
-
-        return res;
+        return result;
     }
 }
