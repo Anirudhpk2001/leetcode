107b9bc131b4a4f2de046d417ce3b34293a7b7aa
Anirudh Kalghatkar <75170111+Anirudhpk2001@users.noreply.github.com>
2025-11-08 13:11:48 +0530

    Time: 1 ms (99.68%), Space: 46.9 MB (10.16%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,27 +1,20 @@
-import java.util.Arrays;
-
 class Solution {
-    int[] memo;
-
     public int jump(int[] nums) {
-        int n = nums.length;
-        memo = new int[n];
-        Arrays.fill(memo, -1); // Initialize memo with -1 to indicate uncomputed states
-        return dp(nums, 0);
-    }
+        int low = 0;
+        int high = 0;
+        int min_steps = 0;
+        for(int i=0;i<nums.length-1;i++)
+        {
+            high = Math.max(high,i+nums[i]);
 
-    private int dp(int[] nums, int i) {
-        if (i >= nums.length - 1) return 0; // Reached the end or beyond
-        if (memo[i] != -1) return memo[i]; // Return already computed result
-
-        int minSteps = Integer.MAX_VALUE;
-        for (int j = 1; j <= nums[i]; j++) {
-            int next = dp(nums, i + j);
-            if (next != Integer.MAX_VALUE) {
-                minSteps = Math.min(minSteps, 1 + next);
+            if(i == low)
+            {
+                min_steps++;
+                low = high;
             }
+
         }
 
-        return memo[i] = minSteps;
+        return min_steps;
     }
 }
