cf63b3e47d8dcc4ffaeb95a14607d94b76eab99d
Anirudh Kalghatkar <75170111+Anirudhpk2001@users.noreply.github.com>
2025-11-09 04:22:21 +0530

    Time: 0 ms (100%), Space: 48.6 MB (9.63%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,33 +1,22 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
+        
         if(nums.length<=2)
         {
             return nums.length;
         }
+        int k = 2;
 
-       
-        int j = 1; // pointer to keep track of the position where we can place the next non-duplicate element
-        int count = 1; // count of occurrences of the current element
-        
-        // Start iterating from the second element
-        for (int i = 1; i < nums.length; i++) {
-            // If the current element is equal to the previous element, increment the count
-            if (nums[i] == nums[i - 1]) {
-                count++;
-            } else {
-                // If the current element is different from the previous element, reset the count to 1
-                count = 1;
-            }
-            
-            // If the count is less than or equal to 2, copy the current element to the next available position
-            if (count <= 2) {
-                nums[j] = nums[i];
-                j++;
+        for(int i=2;i<nums.length;i++)
+        {
+            if(nums[i]!=nums[k-2])
+            {
+                nums[k] = nums[i];
+                k++;
             }
         }
-        
-        // After processing all elements, return the length of the modified array
-        return j;
-      
+
+        return k;
+
     }
 }
