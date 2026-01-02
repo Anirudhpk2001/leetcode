528656f7f27abd5c0be7b0ba9c759a191dc90017
Anirudh Kalghatkar <75170111+Anirudhpk2001@users.noreply.github.com>
2026-01-02 07:43:08 +0530

    Time: 12 ms (49.41%), Space: 46.1 MB (65.02%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        //ABCC k =2
+        int left = 0;
+        int right = 0;
+        int[] alphabet = new int[26];
+        int max_count = 0;
+        int result = 0;
+
+
+        while(right<s.length())
+        {
+            alphabet[s.charAt(right) - 'A']++;
+
+            max_count = Math.max(max_count,alphabet[s.charAt(right)-'A']);
+
+            if(right - left + 1 - max_count > k)
+            {
+                alphabet[s.charAt(left) - 'A']--;
+                left++;
+            }
+
+            result = Math.max(result,right-left+1);
+            right++;
+
+        }
+        
+
+        return result;
+    }
+}
