ecc5990b16487a5bc9da5284abe9e61ef05fb1cf
Anirudh Kalghatkar <75170111+Anirudhpk2001@users.noreply.github.com>
2026-01-23 02:19:39 +0530

    Time: 10 ms (53.09%), Space: 47 MB (22.81%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,5 +1,6 @@
 class Solution {
     int[][] dp;
+    int totalSum;
     private int recursion(int[] nums,int target,int index,int sum)
     {
         if(index == nums.length)
@@ -7,13 +8,24 @@ class Solution {
             return (sum == target)? 1:0;
         }
 
+        if(dp[index][totalSum + sum] != Integer.MIN_VALUE)
+        {
+            return dp[index][totalSum+sum];
+        }
+
         int plus = recursion(nums,target,index+1,sum+nums[index]);
         int minus = recursion(nums,target,index+1, sum-nums[index]);
 
-        return plus+minus;
+        return dp[index][totalSum+sum] =  plus+minus;
     }
     public int findTargetSumWays(int[] nums, int target) {
-      
+        totalSum = Arrays.stream(nums).sum();
+        dp = new int[nums.length][2*totalSum+1];
+
+        for(int[] row : dp)
+        {
+            Arrays.fill(row,Integer.MIN_VALUE);
+        }
         return recursion(nums,target,0,0);
     }
 }
