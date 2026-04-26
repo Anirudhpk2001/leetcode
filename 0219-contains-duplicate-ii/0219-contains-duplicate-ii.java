8236847165b55c0aa0fbcdd9199628213d3eca8b
Anirudh Kalghatkar <75170111+Anirudhpk2001@users.noreply.github.com>
2026-04-26 01:32:30 -0700

    Time: 24 ms (56.95%), Space: 90.4 MB (40.59%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -4,25 +4,18 @@ class Solution {
 
         for(int i=0;i<nums.length;i++)
         {
-            if(!map.isEmpty() && map.containsKey(nums[i]) && Math.abs(map.get(nums[i]) - i) <= k )
+            if(map.containsKey(nums[i]))
             {
-                return true;
-
+                System.out.println("here");
+                if(Math.abs(map.get(nums[i]) - i) <= k )
+                {
+                    return true;
+                }
+                
             }
-
             map.put(nums[i],i);
-
         }
 
         return false;
-    }
-    
-    static {
-    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-        try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
-            fw.write("0");
-        } catch (Exception e) {
-        }
-    }));
     }
 }
