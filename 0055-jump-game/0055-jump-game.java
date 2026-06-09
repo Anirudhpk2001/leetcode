ca84682ef95d1b7e26ec393539dc5c3f4242a2d6
Anirudh Kalghatkar <75170111+Anirudhpk2001@users.noreply.github.com>
2026-06-08 20:14:10 -0700

    Time: 1 ms (99.97%), Space: 47.5 MB (94.8%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,44 +1,15 @@
 class Solution {
-    int[] dp;
-    private boolean solve(int nums[],int index)
-    {
-        if(index>=nums.length)
-        {
-            return false;
-        }
-        if(index==nums.length-1)
-        {
-            return true;
-        }
-        if(dp[index]>-1)
-        {
-            return dp[index]==1 ? true : false; 
-        }
-        boolean result=false;
-        for(int i=1;i<=nums[index];i++)
-        {
-            if(solve(nums,index+i)==true)
-            {
-                result = true;
-                break; 
-            }
-            
-        }
-
-        if(result==true)
-        {
-            dp[index]=1;
-        }
-        else
-        {
-            dp[index]=0;
-        }
-        return result;
-    }
     public boolean canJump(int[] nums) {
-        dp=new int[nums.length+1];
+        int goal = nums.length-1;
 
-        Arrays.fill(dp,-1);
-        return solve(nums,0);
+        for(int i = nums.length-1;i>=0;i--)
+        {
+            if(i+nums[i] >= goal)
+            {
+                goal = i;
+            }
+        }
+
+        return (goal == 0)? true:false;
     }
 }
